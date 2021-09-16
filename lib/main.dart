0c439a7b2c215f71cb749034070e9ee60b5e3be5
Peter Syromiatnikov <feedr.dev@gmail.com>
2021-09-16 11:10:09 -0700

    Basic gameplay functionality without the winning strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tic Tac Toe',
       theme: ThemeData(
-        primarySwatch: Colors.lime,
+        primarySwatch: Colors.green,
       ),
       home: const HomePage(title: 'Tic Tac Toe'),
     );
@@ -30,6 +31,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Board _board = Board();
+  Player _player1 = Player(isX: true);
+  Player _player2 = Player(isX: false);
+  Player? _winner;
+
+  void _handleMove(Position position, Player player) {
+    if (_winner != null) return; // game over
+    if (position.player != null) return; // position is not empty
+    setState(() {
+      position.player = _player1;
+      _player2.nextMove(_board);
+      _checkGameOver();
+    });
+  }
+
+  void _checkGameOver() {
+    final player1Positions =
+        _board.positions.where((p) => p.player == _player1).map((e) => e.id);
+    if (_has3InARow(player1Positions)) {
+      _winner = _player1;
+    } else {
+      final player2Positions =
+          _board.positions.where((p) => p.player == _player2).map((e) => e.id);
+      if (_has3InARow(player2Positions)) {
+        _winner = _player2;
+      }
+    }
+  }
+
+  bool _has3InARow(Iterable<int> positions) {
+    final set = positions.toSet();
+    return set.containsAll([1, 2, 3]) ||
+        set.containsAll([4, 5, 6]) ||
+        set.containsAll([7, 8, 9]) ||
+        set.containsAll([1, 4, 7]) ||
+        set.containsAll([2, 5, 8]) ||
+        set.containsAll([3, 6, 9]) ||
+        set.containsAll([1, 5, 9]) ||
+        set.containsAll([3, 5, 7]);
+  }
+
   @override
   Widget build(BuildContext context) {
     const size = 80.0;
@@ -37,43 +79,100 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TileWidget(key: const Key('1'), size: size, onTap: () {}),
-                TileWidget(key: const Key('2'), size: size, onTap: () {}),
-                TileWidget(key: const Key('3'), size: size, onTap: () {}),
-              ],
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (int i = 0; i < 3; i++)
+                    Builder(builder: (context) {
+                      final position = _board.positions[i];
+                      return TileWidget(
+                        key: Key('${position.id}'),
+                        isX: position.player?.isX ?? false,
+                        checked: position.player != null,
+                        size: size,
+                        onTap: () => _handleMove(position, _player1),
+                      );
+                    }),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (int i = 3; i < 6; i++)
+                    Builder(builder: (context) {
+                      final position = _board.positions[i];
+                      return TileWidget(
+                        key: Key('${position.id}'),
+                        isX: position.player?.isX ?? false,
+                        checked: position.player != null,
+                        size: size,
+                        onTap: () => _handleMove(position, _player1),
+                      );
+                    }),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (int i = 6; i < 9; i++)
+                    Builder(builder: (context) {
+                      final position = _board.positions[i];
+                      return TileWidget(
+                        key: Key('${position.id}'),
+                        isX: position.player?.isX ?? false,
+                        checked: position.player != null,
+                        size: size,
+                        onTap: () => _handleMove(position, _player1),
+                      );
+                    }),
+                ],
+              ),
+              const SizedBox(height: size),
+            ],
+          ),
+          if (_winner != null)
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: size * 3,
+                height: size * 3,
+                child: Center(
+                  child: Text(
+                    'Game Over\n'
+                    'You ${_winner == _player1 ? 'Won!' : 'Lost :('}',
+                    style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.black54,
+                        height: 2,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                padding: const EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  color: Colors.lime.withOpacity(0.7),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TileWidget(key: const Key('4'), size: size, onTap: () {}),
-                TileWidget(key: const Key('5'), size: size, onTap: () {}),
-                TileWidget(key: const Key('6'), size: size, onTap: () {}),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TileWidget(key: const Key('7'), size: size, onTap: () {}),
-                TileWidget(key: const Key('8'), size: size, onTap: () {}),
-                TileWidget(key: const Key('9'), size: size, onTap: () {}),
-              ],
-            ),
-            SizedBox(height: size),
-          ],
-        ),
+        ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            _board = Board();
+            _winner = null;
+          });
+        },
         tooltip: 'Start Over',
         icon: const Icon(Icons.restart_alt),
-        label: Text('Restart'),
+        label: const Text('New Game'),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
@@ -84,12 +183,14 @@ class TileWidget extends StatelessWidget {
     Key? key,
     required this.size,
     required this.onTap,
+    this.isX = true,
     this.checked = false,
   }) : super(key: key);
 
   final double size;
   final Function() onTap;
   final bool checked;
+  final bool isX;
 
   @override
   Widget build(BuildContext context) {
@@ -101,8 +202,64 @@ class TileWidget extends StatelessWidget {
           width: size,
           height: size,
           color: Colors.grey[300],
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            child: checked
+                ? isX
+                    ? Icon(
+                        Icons.clear_rounded,
+                        size: size,
+                        color: Colors.orange[600],
+                      )
+                    : Icon(
+                        Icons.circle_outlined,
+                        size: 0.8 * size,
+                        color: Colors.grey[600],
+                      )
+                : Container(),
+          ),
         ),
       ),
     );
   }
+}
+
+class Board {
+  List<Position> positions = [
+    Position(1),
+    Position(2),
+    Position(3),
+    Position(4),
+    Position(5),
+    Position(6),
+    Position(7),
+    Position(8),
+    Position(9),
+  ];
+}
+
+class Position {
+  final int id;
+  Player? player;
+
+  Position(this.id);
+
+  bool isEmpty() => (player == null);
+}
+
+class Player {
+  final bool isX;
+  List<Position> positions = [];
+
+  Player({required this.isX});
+
+  void nextMove(Board board) {
+    final next = board.positions
+        .firstWhere((value) => value.player == null, orElse: () => Position(0));
+    if (next.id > 0) {
+      next.player = this;
+    }
+  }
+
+  Position? _canWin(Board board) {}
 }
